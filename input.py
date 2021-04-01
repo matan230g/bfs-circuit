b3ea92ae38ae0dba352db6b6bda2acff82b55c6e
matan gadasi <matan230g@gmail.com>
2021-04-01 16:10:52 +0300

    changes

--- input.py
+++ input.py
@@ -2,10 +2,16 @@
 
 
 class Input:
-    def __init__(self, name, value):
-        self.name
-        self.value=value
-        self.conecations=[]
+    def __init__(self, name, value=None):
+        self.name = name
+        self.value = value
+        self.connections = []
+
+    def add_connections_to_gate(self,gate):
+        self.connections.add(gate)
+
+    def set_value(self, value ):
+        self.value = value
 
 
 
--- output.py
+++ output.py
@@ -1,3 +1,7 @@
 class Output:
-    def __init__(self, name):
+    def __init__(self, name, value=None):
         self.name = name
+        self.value = value
+
+    def set_value(self, value):
+        self.value = value
